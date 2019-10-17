d3c2cdeb38638abe04cc3f670b626e0a66ef1753
chris <chris.castor.us@gmail.com>
2019-10-17 12:56:20 -0400

    renaming

--- java/com/deceus/interview/Chess.java
+++ java/com/deceus/interview/Chess.java
@@ -1,8 +1,27 @@
-package com.deceus.interview
-
 import java.util.*;
 
-public class Chess {
+public class ChessMainClass
+{
+  
+  public static void main(String[] args)
+  {
+    ChessOtherClass myObject = new ChessOtherClass("Hello Other Chess Class!");
+    System.out.print(myObject);
+  }
+}
 
-   
+
+public class ChessOtherClass
+{
+  private String message;
+
+  public ChessOtherClass(String input)
+  {
+    message = "Why, " + input + " Isn't this something?";
+  }
+  
+  public String toString()
+  {
+    return message;
+  }
 }
